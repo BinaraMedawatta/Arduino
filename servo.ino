755de1203015e1f99c985d4004f1efb3cf640253
Binara Medawatta <binaramedawatta@gmail.com>
2018-10-13 21:35:36 +0530

    Added servo + input

---
+++ servo.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo myservo; 
+int val;
+void setup() {
+  Serial.begin(115200);
+  myservo.attach(9);
+}
+
+void loop() {
+  val = Serial.read();
+  if(val!=0){
+    myservo.write(pos);
+  }
+}
+
